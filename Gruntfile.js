e68d76c7578d67c1b97bcc191862e6e31ac88da2
Aleksei <me.aleksei@gmail.com>
2015-03-22 19:33:04 -0700

    Add css and change folder structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,67 +1,95 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+    grunt.initConfig({
 
-		// Import package manifest
-		pkg: grunt.file.readJSON("package.json"),
+        // Import package manifest
+        pkg: grunt.file.readJSON("package.json"),
 
-		// Banner definitions
-		meta: {
-			banner: "/*\n" +
-				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
-				" *  <%= pkg.description %>\n" +
-				" *  <%= pkg.homepage %>\n" +
-				" *  Made by <%= pkg.author.name %>\n" +
-				" */\n"
-		},
+        // Banner definitions
+        meta: {
+            banner: "/*\n" +
+                " *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+                " *  <%= pkg.description %>\n" +
+                " *  <%= pkg.homepage %>\n" +
+                " *  Made by <%= pkg.author.name %>\n" +
+                " */\n"
+        },
 
-		// Concat definitions
-		concat: {
-			options: {
-				banner: "<%= meta.banner %>"
-			},
-			dist: {
-				src: ["src/symbolic.js"],
-				dest: "dist/symbolic.js"
-			}
-		},
+        // Concat definitions
+        concat: {
+            options: {
+                banner: "<%= meta.banner %>"
+            },
+            dist: {
+                files: [
+                    {
+                        src: ["src/js/symbolic.js"],
+                        dest: "dist/js/symbolic.js"
+                    },
+                    {
+                        src: ["src/css/symbolic.css"],
+                        dest: "dist/css/symbolic.css"
+                    },
+                    {
+                        src: ["dist/js/symbolic.min.js"],
+                        dest: "dist/js/symbolic.min.js"
+                    },
+                    {
+                        src: ["dist/css/symbolic.min.css"],
+                        dest: "dist/css/symbolic.min.css"
+                    }
+                ]
+            }
+        },
 
-		// Lint definitions
-		jshint: {
-			files: ["src/symbolic.js"],
-			options: {
-				jshintrc: ".jshintrc"
-			}
-		},
+        // Lint definitions
+        jshint: {
+            files: ["src/js/**/*.js"],
+            options: {
+                jshintrc: ".jshintrc"
+            }
+        },
 
-		// Minify definitions
-		uglify: {
-			my_target: {
-				src: ["dist/symbolic.js"],
-				dest: "dist/symbolic.min.js"
-			},
-			options: {
-				banner: "<%= meta.banner %>"
-			}
-		},
+        // Minify definitions
+        uglify: {
+            my_target: {
+                src: ["src/js/symbolic.js"],
+                dest: "dist/js/symbolic.min.js"
+            }
+        },
 
-		// watch for changes to source
-		// Better than calling grunt a million times
-		// (call 'grunt watch')
-		watch: {
-		    files: ['src/*'],
-		    tasks: ['default']
-		}
+        // watch for changes to source
+        // Better than calling grunt a million times
+        // (call 'grunt watch')
+        watch: {
+            files: ['src/**'],
+            tasks: ['default']
+        },
 
-	});
+        // Minify CSS
+        cssmin: {
+            my_target: {
+                src: 'src/css/symbolic.css',
+                dest: 'dist/css/symbolic.min.css'
+            }
+        },
 
-	grunt.loadNpmTasks("grunt-contrib-concat");
-	grunt.loadNpmTasks("grunt-contrib-jshint");
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-watch");
+        csslint: {
+            my_target: {
+                src: ['src/css/**/*.css']
+            }
+        }
+    });
 
-	grunt.registerTask("build", ["concat", "uglify"]);
-	grunt.registerTask("default", ["jshint", "build"]);
-	grunt.registerTask("travis", ["default"]);
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+
+    grunt.registerTask("build", ["uglify", "cssmin", "concat"]);
+    grunt.registerTask("default", ["jshint", "csslint", "build"]);
+    grunt.registerTask("travis", ["default"]);
 
 };
